aaa13626ebd312e3118d8e58bc24259c99d9387a
GiadaGallitto <gallitto.giada@gmail.com>
2023-01-16 11:18:34 +0100

    Add function to get Flag

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,19 +1,57 @@
     <script>
+    import { store } from "../store.js";
+    import axios from "axios";
+
     export default {
+      data(){
+        return {
+          store,
+          flag: ""
+        }
+      },
+
       props: {
         title: String,
         originalTitle: String,
         language: String,
         vote: String,
+      },
+
+      methods: {
+        getFlag(lang){
+          const apiUrl = (lang == "" || lang == "en") ? `https://countryflagsapi.com/png/gb` : `https://countryflagsapi.com/png/${lang}`
+          axios.get(apiUrl, {
+                    params: {
+                        
+                    }
+                })
+                .then((response) => {
+                    console.log(response);
+                    this.flag = apiUrl
+                    console.log(this.flag)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+            }
+          // getFlag(lang) {
+          //   const apiUrl = (lang == "en") ? `https://countryflagsapi.com/png/gb` : `https://countryflagsapi.com/png/${lang}`
+          //   this.flag = apiUrl
+          //   console.log(this.flag)
+          // }
+        },
+
+        created(){
+          this.getFlag(this.language)
+        }
       }
-    }
     </script>
 
 <template>
   <div class="card">
     <h3>{{ title }}</h3>
     <h5>{{ originalTitle }}</h5>
-    <p>{{ language }}</p>
+    <img :src="flag" >
     <span>{{ vote }}</span>
   </div>
 </template>
